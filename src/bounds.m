ad98c5c73cdbf6246d0ac546450a19f8c6edccd2
Jason K. Moore <moorepants@gmail.com>
2019-07-29 07:35:34 -0700

    Simplified par bounds function.

--- src/bounds.m
+++ src/bounds.m
@@ -1,52 +1,35 @@
-function [lb, ub] = bounds()
-% BOUNDS - Returns the lower and upper bounds of the free parameters.
+function [lower, upper] = free_par_bounds()
+% FREE_PAR_BOUNDS - Returns the lower and upper bounds of the free
+% parameters.
+%
+% Outputs:
+%   lower - 23x1 double
+%   upper - 23x1 double
 
-lb = [
- -inf; % w
- -inf; % c
- -pi/2; % lam
- 0.0; % mD
- -inf; % xD
- -inf; % zD
- -inf; % kDxx
- -inf; % kDzz
- -pi/2; % alphaD
- -inf; % xP
- -inf; % zP
- -pi/2; % alphaP
- 0.0; % mH
- -inf; % xH
- -inf; % zH
- -inf; % kHxx
- -inf; % kHyy
- -inf; % kHzz
- -pi/2; % alphaH
- 0.127; % rR (10" OD wheel)
- 1.0; % mR
- 0.127; % rF (10" OD wheel)
- 1.0]; % mF
+bounds = [
+  -inf, inf;  % w
+  -inf, inf;  % c
+ -pi/2, pi/2; % lam
+   0.0, inf;  % mD
+  -inf, inf;  % xD
+  -inf, 0.0;  % zD
+  -inf, inf;  % kDxx
+  -inf, inf;  % kDzz
+ -pi/2, pi/2; % alphaD
+  -inf, inf;  % xP
+  -inf, 0.0;  % zP
+ -pi/2, pi/2; % alphaP
+   0.0, inf;  % mH
+  -inf, inf;  % xH
+  -inf, 0.0;  % zH
+  -inf, inf;  % kHxx
+  -inf, inf;  % kHyy
+  -inf, inf;  % kHzz
+ -pi/2, pi/2; % alphaH
+ 0.127, inf;  % rR (10" OD wheel)
+   1.0, inf;  % mR
+ 0.127, inf;  % rF (10" OD wheel)
+   1.0, inf]; % mF
 
-ub = [
- inf; % w
- inf; % c
- pi/2; % lam
- inf; % mD
- inf; % xD
- 0.0; % zD
- inf; % kDxx
- inf; % kDzz
- pi/2; % alphaD
- inf; % xP
- 0.0; % zP
- pi/2; % alphaP
- inf; % mH
- inf; % xH
- 0.0; % zH
- inf; % kHxx
- inf; % kHyy
- inf; % kHzz
- pi/2; % alphaH
- inf; % rR
- inf; % mR
- inf; % rF
- inf]; % mF
+lower = bounds(:, 1);
+upper = bounds(:, 1);
