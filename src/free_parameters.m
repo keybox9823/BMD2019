87c847e5e0e4c578b4ce115bd9a0a2116f2037a1
Jason K. Moore <moorepants@gmail.com>
2019-07-29 08:19:38 -0700

    Formatting, indentation.

--- src/free_parameters.m
+++ src/free_parameters.m
@@ -1,28 +1,29 @@
 function x = free_parameters(principal_parameters)
 % FREE_PARAMETERS - Returns the free parameter vector, x, given the full set
 % of principal parameters.
+
 p = principal_parameters;
-x = [
-p.w;
-p.c;
-p.lam;
-p.mD;
-p.xD;
-p.zD;
-p.kDxx;
-p.kDzz;
-p.alphaD;
-p.xP;
-p.zP;
-p.alphaP;
-p.mH;
-p.xH;
-p.zH;
-p.kHxx;
-p.kHyy;
-p.kHzz;
-p.alphaH;
-p.rR;
-p.mR;
-p.rF;
-p.mF];
+
+x = [p.w;
+     p.c;
+     p.lam;
+     p.mD;
+     p.xD;
+     p.zD;
+     p.kDxx;
+     p.kDzz;
+     p.alphaD;
+     p.xP;
+     p.zP;
+     p.alphaP;
+     p.mH;
+     p.xH;
+     p.zH;
+     p.kHxx;
+     p.kHyy;
+     p.kHzz;
+     p.alphaH;
+     p.rR;
+     p.mR;
+     p.rF;
+     p.mF];
