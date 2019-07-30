4a0fd1565825ba04504f5c95f74098fbe96d8a4d
Jason K. Moore <moorepants@gmail.com>
2019-07-30 13:05:24 -0700

    Added a function to compute mass centers of an arbitrary number of bodies.

---
+++ src/combine_mass_centers.m
@@ -0,0 +1,13 @@
+function mass_center = combine_mass_centers(masses, mass_centers)
+% COMBINE_MASS_CENTERS - Returns a vector giving the mass center the
+% combined bodies.
+%
+% Inputs:
+%   masses - 1xn double, masses of the n bodies
+%   mass_centers - 3xn double, Cartesian coordinates of the n bodies' mass
+%   centers
+%
+% Outputs:
+%   com - 3x1 double, Cartesian coordinates of the combined bodies mass
+%   center
+mass_center = sum(masses .* mass_centers, 2) ./ sum(masses);
