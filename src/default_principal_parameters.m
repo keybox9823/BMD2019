bddd36106cfe0a3512d3d6a881859abb52a1ad98
Jason K. Moore <moorepants@gmail.com>
2019-07-30 16:25:18 -0700

    Transferred over Jason on Browser parameters from BicycleParameters.

--- src/default_principal_parameters.m
+++ src/default_principal_parameters.m
@@ -1,42 +1,57 @@
 function p = default_principal_parameters()
 
-% based on Jason sitting on the Pista
+% based on Jason sitting on the Browser (parameters taken from
+% BicycleParameters/data)
 
-p.w = 0.989
-p.c = 0.0617300113263
+p.c = 0.0685808540382
+p.w = 1.121
+p.lam = 0.399680398707
 p.g = 9.81
-p.v = 5.0
-p.lam = 0.275762021815
-p.mH = 2.27
-p.mR = 1.38
-p.mF = 1.58
-p.rR = 0.332088156971
-p.rF = 0.333838861345
-p.xH = 0.906321313298
-p.zH = -0.732376356772
+p.v = 3.0
+
+p.mR = 3.11
+p.rR = 0.340958858855
+% TODO : convert to kR11, kR22, kR33
+p.IRxx = 0.0904114316323
+p.IRyy = 0.152391250767
+p.IRzz = p.IRxx;
+
+p.mF = 2.02
+p.rF = 0.34352982332
+% TODO : convert to kF11, kF22, kF33
+p.IFxx = 0.0883826870796
+p.IFyy = 0.149221207336
+p.IFzz = p.IFxx;
+
 % TODO : pick some reasonable values for these two.
-p.lP = nan;
-p.wP = nan;
+p.lP = 1.728;  % estimated from the BicycleParameters figure
+p.wP = 0.483;  % estimated from the BicycleParameters figure
+mP = 72.0
+xP = 0.2909
+yP = 0.0
+zP = -1.1091
+IPxx = 7.9985
+IPxz = -1.9272
+IPyy = 8.0689
+IPzz = 2.3624
 
 % TODO : Put into principal radius of gyration form.
-IFxx = 0.0553476418952
-IFyy = 0.106185287785
-IRxx = 0.0552268228143
-IRyy = 0.0764034890242
+p.mH = 3.22
+p.xH = 0.866949640247
+p.yH = 0.0
+p.zH = -0.748236400835
+p.IHxx = 0.25335539588
+p.IHxz = -0.0720217263293
+p.IHyy = 0.245827908036
+p.IHzz = 0.0955686343473
 
 % TODO : Put into principal radius of gyration form.
-IHxx = 0.0979884309288
-IHxz = -0.00440828467861
-IHyy = 0.0691907586693
-IHzz = 0.0396107377431
-
-% TODO: Break into separate bicycle and rider and put in principal radius of
-% gyration form
-mB = 4.49
-xB = 0.382963782674
-yB = 0.0
-zB = -0.476798507146
-IBxx = 0.289968529291
-IBxz = 0.0502317583637
-IByy = 0.475817094274
-IBzz = 0.249418014295
+% rear frame (only) parameters
+mD = 9.86
+xD = 0.275951285677
+yD = 0.0
+zD = -0.537842424305
+IDxx = 0.52962890621
+IDxz = -0.116285607878
+IDyy = 1.3163960125
+IDzz = 0.756786895402
