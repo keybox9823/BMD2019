d2c34a8600663be5cd12ba4fab6dfc1b93e89b59
Jason K. Moore <moorepants@gmail.com>
2019-07-30 17:40:50 -0700

    Swapped out the xyx inertias for the radii of gyration principal versions in default parameters.

--- src/default_principal_parameters.m
+++ src/default_principal_parameters.m
@@ -3,55 +3,67 @@ function p = default_principal_parameters()
 % based on Jason sitting on the Browser (parameters taken from
 % BicycleParameters/data)
 
-p.c = 0.0685808540382
-p.w = 1.121
-p.lam = 0.399680398707
-p.g = 9.81
-p.v = 3.0
+% geometry and other
+p.c = 0.0685808540382;
+p.w = 1.121;
+p.lam = 0.399680398707;
+p.g = 9.81;
+p.v = 3.0;
 
-p.mR = 3.11
-p.rR = 0.340958858855
-% TODO : convert to kR11, kR22, kR33
-p.IRxx = 0.0904114316323
-p.IRyy = 0.152391250767
-p.IRzz = p.IRxx;
+% rear wheel
+p.mR = 3.11;
+p.rR = 0.340958858855;
+p.xR = 0.0;
+p.yR = 0.0;
+p.zR = -p.rR;
+IRxx = 0.0904114316323;
+IRyy = 0.152391250767;
+IRzz = IRxx;
+p.kR11 = sqrt(IRyy/p.mR);
+p.kR22 = sqrt(IRxx/p.mR);
+p.kR33 = sqrt(IRzz/p.mR);
 
-p.mF = 2.02
-p.rF = 0.34352982332
-% TODO : convert to kF11, kF22, kF33
-p.IFxx = 0.0883826870796
-p.IFyy = 0.149221207336
-p.IFzz = p.IFxx;
+% front wheel
+p.mF = 2.02;
+p.rF = 0.34352982332;
+p.xF = p.w;
+p.yF = 0.0;
+p.zF = -p.rF;
+IFxx = 0.0883826870796;
+IFyy = 0.149221207336;
+IFzz = IFxx;
+p.kF11 = sqrt(IFyy/p.mF);
+p.kF22 = sqrt(IFxx/p.mF);
+p.kF33 = sqrt(IFzz/p.mF);
 
-% TODO : pick some reasonable values for these two.
+% person
 p.lP = 1.728;  % estimated from the BicycleParameters figure
 p.wP = 0.483;  % estimated from the BicycleParameters figure
-p.mP = 83.50000000000001
-p.xP = 0.3157679154070924
-p.yP = 0.0
-p.zP = -1.0989885659819278
-p.IPxx = 10.985244511152501
-p.IPyy = 11.257100519570363
-p.IPzz = 2.2692214245606905
-p.IPxz = -1.7021081054955438
+p.mP = 83.50000000000001;
+p.xP = 0.3157679154070924;
+p.yP = 0.0;
+p.zP = -1.0989885659819278;
+p.kP11 = 0.36796654;
+p.kP22 = 0.36717241;
+p.kP33 = 0.15276369;
+p.alphaP = -0.18617531745505142;
 
-% TODO : Put into principal radius of gyration form.
-p.mH = 3.22
-p.xH = 0.866949640247
-p.yH = 0.0
-p.zH = -0.748236400835
-p.IHxx = 0.25335539588
-p.IHxz = -0.0720217263293
-p.IHyy = 0.245827908036
-p.IHzz = 0.0955686343473
+% front frame
+p.mH = 3.22;
+p.xH = 0.866949640247;
+p.yH = 0.0;
+p.zH = -0.748236400835;
+p.kH11 = 0.29556015;
+p.kH22 = 0.27630431;
+p.kH33 = 0.14493343;
+p.alphaH = -0.3699481738478786;
 
-% TODO : Put into principal radius of gyration form.
 % rear frame (only) parameters
-mD = 9.86
-xD = 0.275951285677
-yD = 0.0
-zD = -0.537842424305
-IDxx = 0.52962890621
-IDxz = -0.116285607878
-IDyy = 1.3163960125
-IDzz = 0.756786895402
+p.mD = 9.86;
+p.xD = 0.275951285677;
+p.yD = 0.0;
+p.zD = -0.537842424305;
+p.kD11 = 0.36538846;
+p.kD22 = 0.28586689;
+p.kD33 = 0.22079007;
+p.alphaD = -1.1722101094171953;
