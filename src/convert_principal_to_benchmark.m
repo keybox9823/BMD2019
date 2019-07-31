e3de4b50a34ba8bc154c90dc0dd8ed82d3e3f4b1
Jason K. Moore <moorepants@gmail.com>
2019-07-30 19:06:39 -0700

    Fixed coding typos, now runs in matlab with HumanControl code.

--- src/convert_principal_to_benchmark.m
+++ src/convert_principal_to_benchmark.m
@@ -81,7 +81,7 @@ b.v = p.v;
 % rear frame [B]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 b.mB = p.mD + p.mP;
-Bcom = combine_mass_centers([p.mD, p.mP],
+Bcom = combine_mass_centers([p.mD, p.mP], ...
                             [[p.xD; p.yD; p.zD], [p.xP; p.yD; p.zP]]);
 b.xB = Bcom(1);
 b.yB = Bcom(2);
--- src/update_principal_parameters.m
+++ src/update_principal_parameters.m
@@ -10,8 +10,8 @@ p.lam    = x(3, 1);
 p.mD     = x(4, 1);
 p.xD     = x(5, 1);
 p.zD     = x(6, 1);
-p.kDxx   = x(7, 1);
-p.kDzz   = x(8, 1);
+p.kD11   = x(7, 1);
+p.kD33   = x(8, 1);
 p.alphaD = x(9, 1);
 p.xP     = x(10, 1);
 p.zP     = x(11, 1);
@@ -19,11 +19,17 @@ p.alphaP = x(12, 1);
 p.mH     = x(13, 1);
 p.xH     = x(14, 1);
 p.zH     = x(15, 1);
-p.kHxx   = x(16, 1);
-p.kHyy   = x(17, 1);
-p.kHzz   = x(18, 1);
+p.kH11   = x(16, 1);
+p.kH22   = x(17, 1);
+p.kH33   = x(18, 1);
 p.alphaH = x(19, 1);
 p.rR     = x(20, 1);
 p.mR     = x(21, 1);
 p.rF     = x(22, 1);
 p.mF     = x(23, 1);
+
+% TODO : These can be added as constraints, no? Or these shouldn't be
+% principal parameters.
+p.zR = -p.rR;
+p.xF = p.w;
+p.zF = -p.rF;
