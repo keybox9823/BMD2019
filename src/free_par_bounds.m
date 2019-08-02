b365d089effd087def9ece97ea27ca6a369206ba
Jason K. Moore <moorepants@gmail.com>
2019-08-02 12:59:49 -0700

    Added minmum masses and radii of gyration.

--- src/free_par_bounds.m
+++ src/free_par_bounds.m
@@ -10,21 +10,21 @@ bounds = [
   -inf, inf;  % w
   -inf, inf;  % c
  -pi/2, pi/2; % lam
-   0.0, inf;  % mD
+   1.0, inf;  % mD
   -inf, inf;  % xD
   -inf, 0.0;  % zD
-  -inf, inf;  % kDaa
-  -inf, inf;  % kDbb
+   0.0, inf;  % kDaa
+   0.0, inf;  % kDbb
  -pi/2, pi/2; % alphaD
   -inf, inf;  % xP
   -inf, 0.0;  % zP
  -pi/2, pi/2; % alphaP
-   0.0, inf;  % mH
+  0.25, inf;  % mH
   -inf, inf;  % xH
   -inf, 0.0;  % zH
-  -inf, inf;  % kHaa
-  -inf, inf;  % kHbb
-  -inf, inf;  % kHyy
+   0.0, inf;  % kHaa
+   0.0, inf;  % kHbb
+   0.0, inf;  % kHyy
  -pi/2, pi/2; % alphaH
  0.127, inf;  % rR (10" OD wheel)
    1.0, inf;  % mR
