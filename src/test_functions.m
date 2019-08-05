f0f37903662d57739b39cf8aeb6f02108e1dba4a
Jason K. Moore <moorepants@gmail.com>
2019-08-05 00:17:57 -0700

    Change compute_constraint test so that it cna run in octave.

--- src/test_functions.m
+++ src/test_functions.m
@@ -45,4 +45,4 @@ p_updated = update_principal_parameters(p, nan*ones(23,1))
 
 [lb, ub] = free_par_bounds()
 
-[c, ceq] = compute_constraints(x, p)
+[c, ceq] = compute_constraints(x, p, -ones(12, 1))
