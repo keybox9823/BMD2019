31a6bb86ccf1ee2dec61aa1b13676687e503032e
Jason K. Moore <moorepants@gmail.com>
2019-08-21 09:30:42 -0700

    Stacked the geometry plots.

--- src/geometry_plot.py
+++ src/geometry_plot.py
@@ -5,16 +5,15 @@ from bicycleparameters.parameter_sets import PrincipalParameterSet
 
 speeds = [3, 5, 7, 9]
 
-fig, axes = plt.subplots(nrows=3, ncols=len(speeds)+1, sharey=True,
-                         gridspec_kw={'width_ratios': [1, 2, 1.5, 1.5, 4]})
+fig, axes = plt.subplots(nrows=3, ncols=2)
 
 with open('data/principal-browserjason.yml', 'r') as f:
     par_dict = yaml.load(f)['values']
     par_set = PrincipalParameterSet(par_dict)
-    par_set.plot_all(ax=axes[0])
-    axes[0].set_title('Reference')
+    par_set.plot_all(ax=axes[0, 0])
+    axes[0, 0].set_title('Reference')
 
-for ax, speed in zip(axes[1:], speeds):
+for ax, speed in zip(axes.flatten()[2:], speeds):
 
     tail = 'a' if speed == 3 else 'best'
 
@@ -22,13 +21,11 @@ for ax, speed in zip(axes[1:], speeds):
         par_dict = yaml.load(f)['values']
 
     par_set = PrincipalParameterSet(par_dict)
-    #par_set.plot_geometry(ax=ax)
     par_set.plot_all(ax=ax)
-    #ax.set_aspect('equal')
     ax.set_title('{} m/s'.format(speed))
     ax.set_ylabel('')
 
-width = 12.0
-fig.set_size_inches((width, 500/1900*width))
+width = 8.0
+fig.set_size_inches((width, 4/3*width))
 fig.tight_layout()
 fig.savefig('figures/optimal-geometries.png', dpi=300)
