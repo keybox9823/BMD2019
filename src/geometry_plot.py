e1a07b1d1584bb92db947f6dc1cfe4f9517948ce
Jason K. Moore <moorepants@gmail.com>
2019-08-21 22:30:11 -0700

    Plot each indiviaul bike for close look.

--- src/geometry_plot.py
+++ src/geometry_plot.py
@@ -10,7 +10,9 @@ fig, axes = plt.subplots(nrows=3, ncols=2)
 with open('data/principal-browserjason.yml', 'r') as f:
     par_dict = yaml.load(f)['values']
     par_set = PrincipalParameterSet(par_dict)
-    par_set.plot_all(ax=axes[0, 0])
+    bpar_set = par_set.to_benchmark()
+    bpar_set.plot_all(ax=axes[0, 0])
+    par_set.plot_person_diamond(ax=axes[0, 0])
     axes[0, 0].set_title('Reference')
 
 for ax, speed in zip(axes.flatten()[2:], speeds):
@@ -21,11 +23,27 @@ for ax, speed in zip(axes.flatten()[2:], speeds):
         par_dict = yaml.load(f)['values']
 
     par_set = PrincipalParameterSet(par_dict)
-    par_set.plot_all(ax=ax)
-    ax.set_title('{} m/s'.format(speed))
+    bpar_set = par_set.to_benchmark()
+    bpar_set.plot_all(ax=ax)
+    par_set.plot_person_diamond(ax=ax)
+    ax.set_title('{} m/s Design Speed'.format(speed))
     ax.set_ylabel('')
 
 width = 8.0
 fig.set_size_inches((width, 4/3*width))
 fig.tight_layout()
 fig.savefig('figures/optimal-geometries.png', dpi=300)
+
+
+for speed in speeds:
+
+    fig, ax = plt.subplots()
+    tail = 'a' if speed == 3 else 'best'
+
+    with open('data/principal-extended{}{}.yml'.format(speed, tail), 'r') as f:
+        par_dict = yaml.load(f)['values']
+
+    par_set = PrincipalParameterSet(par_dict)
+    par_set.plot_all(ax=ax)
+    ax.set_title('{} m/s Design Speed'.format(speed))
+    fig.savefig('figures/optimal-geometry-{}.png'.format(speed), dpi=300)
